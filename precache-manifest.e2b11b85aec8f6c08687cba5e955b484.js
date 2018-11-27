09ccf5ceb03940c29c365fe3eb5f2ae215522383
Elizabeth Cottrell <elizabeth.cottrell@thinkcompany.com>
2018-11-27 15:09:57 -0500

    Updates

---
+++ precache-manifest.e2b11b85aec8f6c08687cba5e955b484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "5648062085f0def5345c",
+    "url": "/react-tutorial/static/js/main.56480620.chunk.js"
+  },
+  {
+    "revision": "65ce664db22ebc0891b6",
+    "url": "/react-tutorial/static/js/1.65ce664d.chunk.js"
+  },
+  {
+    "revision": "5648062085f0def5345c",
+    "url": "/react-tutorial/static/css/main.2069dde1.chunk.css"
+  },
+  {
+    "revision": "2754ba0a7a9ff2cc831d326c77aeb741",
+    "url": "/react-tutorial/index.html"
+  }
+];
